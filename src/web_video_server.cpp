c470146f4d10b3085c1ea61c50067b90da61b0d4
Wei Liu <geoliuwei@gmail.com>
2019-01-15 23:45:12 +0800

    fix type format match bug in ros2 (#79)

--- src/web_video_server.cpp
+++ src/web_video_server.cpp
@@ -211,11 +211,11 @@ bool WebVideoServer::handle_list_streams(const async_web_server_cpp::HttpRequest
     auto & topic_type = topic_and_types.second[0];  // explicitly take the first
     // TODO debugging
     fprintf(stderr, "topic_type: %s\n", topic_type.c_str());
-    if (topic_type == "sensor_msgs::msg::Image")
+    if (topic_type == "sensor_msgs/Image")
     {
       image_topics.push_back(topic_name);
     }
-    else if (topic_type == "sensor_msgs::msg::CameraInfo")
+    else if (topic_type == "sensor_msgs/CameraInfo")
     {
       camera_info_topics.push_back(topic_name);
     }
